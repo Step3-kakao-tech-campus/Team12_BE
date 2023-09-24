74134152c51ae4350ca5162c45d7ab4e7dccfc2d
JH <201667@jnu.ac.kr>
2023-09-24 16:49:59 +0900

    feat : 닉네임 중복확인, 회원가입 구현

---
+++ pickupShuttle/src/main/java/pickup_shuttle/pickup/domain/user/UserRepository.java
@@ -0,0 +1,10 @@
+package pickup_shuttle.pickup.domain.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByNickname(String nickname);
+}
---
+++ pickupShuttle/src/main/java/pickup_shuttle/pickup/domain/user/UserRestController.java
@@ -0,0 +1,30 @@
+package pickup_shuttle.pickup.domain.user;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import pickup_shuttle.pickup.domain._core.utils.ApiUtils;
+
+@RequiredArgsConstructor
+@RestController
+public class UserRestController {
+    private final UserService userService;
+    // 회원가입
+    @PostMapping("/users/register")
+    public ResponseEntity<?> register(@RequestBody @Valid UserRequest.RegisterDTO registerDTO){
+        userService.register(registerDTO);
+        return ResponseEntity.ok(ApiUtils.success("회원가입 성공"));
+    }
+
+    // 닉네임 중복 확인
+    @GetMapping("/users/duplicate/nickname")
+    public ResponseEntity<?> checkNickname(@RequestBody @Valid UserRequest.NicknameCheckDTO nicknameCheckDTO){
+        userService.checkNicknameDuplicate(nicknameCheckDTO.getNickname());
+        return ResponseEntity.ok(ApiUtils.success("사용 가능한 닉네임 입니다"));
+    }
+
+}
