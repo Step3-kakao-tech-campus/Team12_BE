cc47d9ef2c25a706e84366660cecfe19100b098c
JH <201667@jnu.ac.kr>
2023-10-15 15:45:01 +0900

    feat : [#55],[#56] 이미지 업로드, presigned URL 요청 (AWS S3)

--- pickupShuttle/src/main/java/pickup_shuttle/pickup/domain/user/UserService.java
+++ pickupShuttle/src/main/java/pickup_shuttle/pickup/domain/user/UserService.java
@@ -1,14 +1,26 @@
 package pickup_shuttle.pickup.domain.user;
 
+import com.amazonaws.HttpMethod;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.Headers;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.GeneratePresignedUrlRequest;
+import com.amazonaws.services.s3.model.ObjectMetadata;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 import pickup_shuttle.pickup._core.errors.exception.Exception400;
+import pickup_shuttle.pickup._core.errors.exception.Exception500;
 import pickup_shuttle.pickup.config.ErrorMessage;
 import pickup_shuttle.pickup.domain.oauth2.CustomOauth2User;
 import pickup_shuttle.pickup.domain.user.dto.request.UserModifyRqDTO;
 import pickup_shuttle.pickup.domain.user.dto.request.SignUpRqDTO;
+import pickup_shuttle.pickup.domain.user.dto.response.UserGetImageUrlRpDTO;
 
+import java.io.InputStream;
+import java.util.Date;
 import java.util.Optional;
 
 @Transactional(readOnly = true)
@@ -16,7 +28,12 @@ import java.util.Optional;
 @Service
 public class UserService {
     private final UserRepository userRepository;
+    private final AmazonS3 amazonS3;
 
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+    @Value("${cloud.aws.s3.dir}")
+    private String dir;
     @Transactional
     public void signup(SignUpRqDTO signUpRqDTO, CustomOauth2User customOauth2User){
         String bankName = signUpRqDTO.bankName();
@@ -65,5 +82,78 @@ public class UserService {
         return userPK;
     }
 
+    @Transactional
+    public void uploadImage(MultipartFile multipartFile, Long userId){
+        // 메타 데이터 설정
+        ObjectMetadata metadata = new ObjectMetadata();
+        metadata.setContentLength(multipartFile.getSize());
+        metadata.setContentType(multipartFile.getContentType());
+        // 파일 읽기
+        InputStream inputStream;
+        try{
+            inputStream = multipartFile.getInputStream();
+        }catch(Exception e){
+            throw new Exception400("파일을 읽을 수 없습니다");
+        }
+        // 유저 검증
+        User user = userRepository.findById(userId).orElseThrow(
+                () -> new Exception400("유저를 찾을 수 없습니다")
+        );
+        if(user.getUserRole().getValue().equals("ROLE_STUDENT"))
+            throw new Exception400("이미 인증된 유저입니다");
+        // 업로드
+        try{
+            String fileName = dir + userId + ".jpg"; // 파일명 : {userId}.jpg
+            amazonS3.putObject(bucket, fileName, inputStream, metadata); // aws
+            user.updateUrl(amazonS3.getUrl(bucket, fileName).toString()); // mysql
+        }catch (Exception e){
+            throw new Exception500("AWS 이미지 업로드를 실패했습니다");
+        }
+    }
+
+    public UserGetImageUrlRpDTO getImageUrl(Long userId){
+        User user = userRepository.findById(userId).orElseThrow(
+                () -> new Exception400("유저를 찾을 수 없습니다")
+        );
+        if(user.getUrl().equals("")){
+            throw new Exception400("등록된 이미지가 존재하지 않습니다");
+        }
+        return UserGetImageUrlRpDTO.builder().url(getPresignedUrl(userId)).build(); // PreSigned URL 응답
+    }
+
+    // PreSigned URL 발급
+    public String getPresignedUrl(Long userId) {
+
+        String fileName = dir + userId + ".jpg";
+        GeneratePresignedUrlRequest generatePresignedUrlRequest = getGeneratePreSignedUrlRequest(bucket, fileName);
+        try{
+            String presignedUrl = amazonS3.generatePresignedUrl(generatePresignedUrlRequest).toString();
+            System.out.println(presignedUrl); // 발급 확인용 (배포 전 삭제)
+            return presignedUrl;
+        }catch (Exception e){
+            throw new Exception500("presigned url 발급을 실패했습니다");
+        }
+    }
+
+    // PreSigned URL(GET) 요청 생성
+    private GeneratePresignedUrlRequest getGeneratePreSignedUrlRequest(String bucket, String fileName) {
+        GeneratePresignedUrlRequest generatePresignedUrlRequest =
+                new GeneratePresignedUrlRequest(bucket, fileName)
+                        .withMethod(HttpMethod.GET)
+                        .withExpiration(getPreSignedUrlExpiration());
+        generatePresignedUrlRequest.addRequestParameter(
+                Headers.S3_CANNED_ACL,
+                CannedAccessControlList.PublicRead.toString());
+        return generatePresignedUrlRequest;
+    }
+
+    // URL 만료 시간
+    private Date getPreSignedUrlExpiration() {
+        Date expiration = new Date();
+        long expTimeMillis = expiration.getTime();
+        expTimeMillis += 1000 * 60 * 5; // 5분
+        expiration.setTime(expTimeMillis);
+        return expiration;
+    }
 
 }
