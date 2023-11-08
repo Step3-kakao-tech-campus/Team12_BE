858e6b01b39ff7ae3ee800677be5c4d5aea468d5
JH <201667@jnu.ac.kr>
2023-11-08 17:39:12 +0900

    feat : BAD_REQUEST에 고정적으로 사용될ErrorMessage 생성

--- src/main/java/pickup_shuttle/pickup/config/ErrorMessage.java
+++ src/main/java/pickup_shuttle/pickup/config/ErrorMessage.java
@@ -4,4 +4,10 @@ public class ErrorMessage {
     public static final String UNKNOWN_USER = "유저를 찾을 수 없습니다";
     public static final String UNKNOWN_BOARD = "공고글을 찾을 수 없습니다";
     public static final String UNKNOWN_STORE = "가게를 찾을 수 없습니다";
+    public static final String BADREQUEST_SIZE = " : 길이는 60이하 이어야합니다";
+    public static final String BADREQUEST_BLANK = " : 공백입니다";
+    public static final String BADREQUEST_MIN = " : 1 이상이어야 합니다";
+    public static final String BADREQUEST_MAX = " : 2147483647 이하이어야 합니다";
+
+
 }
