635dbf98b4a8a24429c3b34900ed0e5c0dca8166
JH <201667@jnu.ac.kr>
2023-11-08 15:36:35 +0900

    feat : 공고 수락 RqDTO의 도착예정 시간 0이상 60이하로 유효성 검사

--- src/main/java/pickup_shuttle/pickup/domain/board/dto/request/BoardAgreeRqDTO.java
+++ src/main/java/pickup_shuttle/pickup/domain/board/dto/request/BoardAgreeRqDTO.java
@@ -1,9 +1,12 @@
 package pickup_shuttle.pickup.domain.board.dto.request;
 
-import jakarta.validation.constraints.PositiveOrZero;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import lombok.Builder;
 
 @Builder
 public record BoardAgreeRqDTO(
-      @PositiveOrZero(message = "도착예정 시간이 음수입니다") int arrivalTime
+        @Min(value = 0, message = "도착예정 시간은 0 이상이어야 합니다")
+        @Max(value = 60, message = "도착예정 시간은 60 이하이어야 합니다")
+        int arrivalTime
 ) { }
